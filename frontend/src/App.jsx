e2ae6e2daffe1cd7114a2efce20b1ca94b2c2fa8
Andrei Valdez <20210022375@my.xu.edu.ph>
2026-03-19 23:46:46 +0800

    Added cards partially

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import './Config.css';
+import './Main.css';
 import { useState, useEffect } from 'react';
 import Pagination from '@mui/material/Pagination';
 import NavBar from './components/navbar/NavBar.jsx';
 import Rooms from './components/rooms/Rooms.jsx';
+import Artifact from './components/artifact/Artifact.jsx';
 
 function App() {
 
@@ -16,12 +17,49 @@ function App() {
     <NavBar/> {/*Pass in searchQuery soon as props*/}
     <Rooms/> {/*Pass in currentRoom soon as props */}
 
+    {/*To optimize soon... as is for now*/}
     <div className="main-content">
-      <div className="first"> Content </div>
-      
+      <div className="gnome-container"> 
+        <div className="move-left move"> 
+          <span aria-hidden="true" className="carousel-control-prev-icon move-icon"></span>
+        </div> 
 
+
+
+        <div className="artifacts-grid"> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+          <Artifact/> 
+
+
+        </div>
+
+
+
+        <div className="move-right move"> 
+          <span aria-hidden="true" className="carousel-control-next-icon move-icon"></span> 
+        </div> 
+      </div>
+      
       <div className="footer"> 
-        <Pagination count={20} shape="rounded" /> {/* Calculate based on table size soon*/}
+        <Pagination count={20} shape="rounded" /> {/* Calculate based on table size soon of artifacts*/}
       </div>
     </div>
   
---
+++ frontend/src/components/artifact/Artifact.jsx
@@ -0,0 +1,19 @@
+import './Artifact.css';
+
+
+function Artifact(){
+
+    return (
+        <div className="card-container">
+            <div className="card-img">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5_-G-aUrLzcrqXAlgkn6nNpNwK3-rs5pT1Q&s"/>
+            </div>
+
+            <div className="card-info">
+                testing
+            </div>
+        </div>
+    );
+}
+
+export default Artifact;
