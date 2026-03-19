20810c4cf81bd627aee58e143e8f649d3ffae3b5
Andrei Valdez <20210022375@my.xu.edu.ph>
2026-03-19 21:28:00 +0800

    Added pagination

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import './Config.css';
 import { useState, useEffect } from 'react';
+import Pagination from '@mui/material/Pagination';
 import NavBar from './components/navbar/NavBar.jsx';
 import Rooms from './components/rooms/Rooms.jsx';
 
@@ -10,9 +12,21 @@ function App() {
 
   return (
     <>
-      <NavBar/> {/*Pass in searchQuery soon as props*/}
-      <Rooms/> {/*Pass in currentRoom soon as props */}
+    
+    <NavBar/> {/*Pass in searchQuery soon as props*/}
+    <Rooms/> {/*Pass in currentRoom soon as props */}
 
+    <div className="main-content">
+      <div className="first"> Content </div>
+      
+
+      <div className="footer"> 
+        <Pagination count={20} shape="rounded" /> {/* Calculate based on table size soon*/}
+      </div>
+    </div>
+  
+
+  
     </>
   );
 }
--- frontend/src/components/rooms/Rooms.jsx
+++ frontend/src/components/rooms/Rooms.jsx
@@ -7,7 +7,25 @@ function Rooms(){
 
 
     return (
-    <Carousel> {/*No delete and edits in this room*/}
+    <Carousel interval={null}> {/*No delete and edits in this room*/}
+      <Carousel.Item>
+        <img className="d-block w-100" src="https://www.xu.edu.ph/images/2016/img/may/xu.jpg" alt="First slide" style={{ height: '200px', width: '100%', objectFit: 'cover' }}/>
+        <Carousel.Caption>
+          <h2 style={{color: '#b99430'}}> The Collection in it's entirety </h2>
+          <h3 style={{color: '#b99430'}}>Museo De Oro</h3>
+          <p style={{color: '#b99430'}}>Museo de Oro, dubbed as the first folkloric museum in the country, is not an abode of dead things. It is, by all means, bursting with life, clad with vivid imagination and rich history.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <img className="d-block w-100" src="https://www.xu.edu.ph/images/2016/img/may/xu.jpg" alt="First slide" style={{ height: '200px', width: '100%', objectFit: 'cover' }}/>
+        <Carousel.Caption>
+          <h2 style={{color: '#b99430'}}> The Collection in it's entirety </h2>
+          <h3 style={{color: '#b99430'}}>Museo De Oro</h3>
+          <p style={{color: '#b99430'}}>Museo de Oro, dubbed as the first folkloric museum in the country, is not an abode of dead things. It is, by all means, bursting with life, clad with vivid imagination and rich history.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+
       <Carousel.Item>
         <img className="d-block w-100" src="https://www.xu.edu.ph/images/2016/img/may/xu.jpg" alt="First slide" style={{ height: '200px', width: '100%', objectFit: 'cover' }}/>
         <Carousel.Caption>
