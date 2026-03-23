594b736766286ebee0a7c9dea959735bbc405b1f
Andrei Valdez <20210022375@my.xu.edu.ph>
2026-03-23 21:03:30 +0800

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,12 +21,11 @@ function App() {
 
     {/*To optimize soon... as is for now*/}
     <div className="main-content">
-
+      <Options/>
       <div className="gnome-container"> 
         <div className="move-left move"> 
           <span aria-hidden="true" className="carousel-control-prev-icon move-icon"></span>
         </div> 
-
 
         <div className="artifacts-grid"> 
           <Artifact/> 
@@ -79,22 +78,16 @@ function App() {
           <Artifact/> 
           <Artifact/> 
           <Artifact/> 
-
         </div>
-
 
         <div className="move-right move"> 
           <span aria-hidden="true" className="carousel-control-next-icon move-icon"></span> 
-          <Options/>
-
         </div> 
       </div>
       
     </div>
   
-
     <Footer/>
-
   
     </>
   );
--- frontend/src/components/options/Options.jsx
+++ frontend/src/components/options/Options.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import SpeedDial from "@mui/material/SpeedDial";
 import SpeedDialAction from "@mui/material/SpeedDialAction";
-import Backdrop from "@mui/material/Backdrop";
 
 function Options() {
   const [open, setOpen] = useState(false);
@@ -9,16 +8,12 @@ function Options() {
   return (
     <div className="options-container">
       
-      <Backdrop open={open} sx={{backdropFilter: "blur(5px)",backgroundColor: "rgba(0,0,0,0.2)",zIndex: 1,}} onClick={() => setOpen(false)}/>
-
-      <SpeedDial ariaLabel="Simple SpeedDial" open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)} style={{ position: "absolute", bottom: 60, right: 50, zIndex: 1}} icon={<span>+</span>}>
+      <SpeedDial ariaLabel="Simple SpeedDial" FabProps={{sx: {backgroundColor: "#283971", color: "#fff", "&:hover": {backgroundColor: "#3a52a3"}}}} open={open} onOpen={() => setOpen(true)} onClose={() => setOpen(false)} style={{ position: "absolute", bottom: 80, right: 50, zIndex: 1,}} icon={<span>+</span>}>
         
-        <SpeedDialAction icon={<span>A</span>} tooltipTitle="Add Artifact" onClick={() => alert("First")}/>
+        <SpeedDialAction icon={<span>A</span>} tooltipTitle="Add Artifact" onClick={() => console.log("Working")}/>
         
         <SpeedDialAction icon={<span>B</span>} tooltipTitle="Head Count" onClick={() => alert("Second")}/>
         
-        <SpeedDialAction icon={<span>C</span>} tooltipTitle="Something Else" onClick={() => alert("Third")}/>
-
       </SpeedDial>
     </div>
   );
