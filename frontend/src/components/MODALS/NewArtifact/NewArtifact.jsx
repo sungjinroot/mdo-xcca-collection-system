f23a214ce91278f0dd376cdc0cd497ac8788a4d9
Andrei Valdez <20210022375@my.xu.edu.ph>
2026-03-27 00:49:16 +0800

    Added stepper to add modal

--- frontend/src/components/MODALS/NewArtifact/NewArtifact.jsx
+++ frontend/src/components/MODALS/NewArtifact/NewArtifact.jsx
@@ -1,27 +1,60 @@
 import Modal from 'react-bootstrap/Modal';
+import { useState } from 'react';
 import './ModalSizeHeight.css';
 import './ModalSizeWidth.css';
+import { StepLabel,Stepper,Step } from '@mui/material';
+
 
 function NewArtifact(props){
+
+    const steps = ['Basic Information','Acquisition','Physical Description','Contact Persons','Images']
+
+    const [currentStep, setCurrentStep] = useState(0);
+
+    const nextStep = () => {
+        if (currentStep < steps.length - 1) {
+            setCurrentStep(currentStep + 1);
+        }
+    };
+
+    const prevStep = () => {
+        if (currentStep > 0) {
+            setCurrentStep(currentStep - 1);
+        }
+    };
+
     
     return (
         <Modal show={props.show} onHide={() => props.setShow(false)} contentClassName="ModalSizeHeight" dialogClassName="ModalSizeWidth" aria-labelledby="example-custom-modal-styling-title">
-            <Modal.Header closeButton>
-                <Modal.Title id="example-custom-modal-styling-title">
-                    New Artifact
-                </Modal.Title>
+            <Modal.Header closeButton style={{ backgroundColor: '#283971' }} className="d-flex align-items-center">
+                <img src="src/assets/logo.png"/>
+
+                <h3 className="mx-auto text-white">New Artifact</h3>
+                
+            
             </Modal.Header>
             
-            <Modal.Body>
-                <p>
-                    Ipsum molestiae natus adipisci modi eligendi? Debitis amet quae unde
-                    commodi aspernatur enim, consectetur. Cumque deleniti temporibus
-                    ipsam atque a dolores quisquam quisquam adipisci possimus
-                    laboriosam. Quibusdam facilis doloribus debitis! Sit quasi quod
-                    accusamus eos quod. Ab quos consequuntur eaque quo rem! Mollitia
-                    reiciendis porro quo magni incidunt dolore amet atque facilis ipsum
-                    deleniti rem!
-                </p>
+            <Modal.Body> 
+
+                <Stepper activeStep={currentStep}>
+                    {steps.map((label) => (
+                        <Step key={label}>
+                            <StepLabel>{label}</StepLabel>
+                        </Step>
+                    ))}
+                </Stepper>
+
+                <button onClick={() => prevStep()}>
+                    Prev
+                </button>
+
+                <button onClick={() => nextStep()}>
+                    Next
+                </button>
+
+                
+              
+
             </Modal.Body>
         </Modal>
   );
