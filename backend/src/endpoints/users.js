17c3e0a674d1720e7dac95e0b1ca9897e4a014c4
Cermund John Alcantara <20210023416@my.xu.edu.ph>
2026-04-28 22:23:33 +0800

    nag add ko users endpoint ug nag update ko sa api.js

---
+++ backend/src/endpoints/users.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const endpoint = express.Router();
+const pool = require('../db');
+
+// GET ALL USERS
+endpoint.get("/", async (req, res) => {
+    try {
+        const result = await pool.query("SELECT * FROM users");
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Users does not exist" });
+    }
+});
+
+// DELETE USER
+endpoint.delete("/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const result = await pool.query(
+            "DELETE FROM users WHERE userID = $1",
+            [id]
+        );
+
+        if (result.rowCount === 0) {
+            return res.status(400).json({ message: "User cannot be deleted" });
+        }
+
+        res.status(200).json({ message: "User deleted" });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: err.message });
+    }
+});
+
+module.exports = endpoint;
