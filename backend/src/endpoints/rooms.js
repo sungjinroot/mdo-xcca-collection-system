dc23429e640c28c211897da7c856ff8bea9fd694
Johnrose Saldua <200810497@my.xu.edu.ph>
2026-05-10 22:47:19 +0800

    Changed delete rooms messages

--- backend/src/endpoints/rooms.js
+++ backend/src/endpoints/rooms.js
@@ -85,6 +85,38 @@ endpoint.delete("/:id", async (req, res) => {
             [id]
         );
         if (result.rowCount === 0) {
+            
+            const checkifroomexists = await pool.query(
+                `SELECT EXISTS(SELECT 1 FROM rooms WHERE roomID = $1) AS room_exists`,
+                [id]
+            );
+
+            const { room_exists } = check.rows[0];
+
+            if (!room_exists){
+                return res.status(404).json({
+                    status: "error",
+                    code: "ROOM_NOT_FOUND",
+                    message: 'Room with ID ${id} does not exists'
+                });
+            }
+            
+
+            return res.status(409).json({
+                status: "error",
+                code: "ROOM_HAS_ARTIFACTS",
+                message: `Room with ID ${id} cannot be deleted because it still has associated artifacts. Please remove all artifacts first.`
+            });
+        }
+
+        res.status(200).json({
+            status: "success",
+            code: "ROOM_DELETED",
+            message: `Room with ID ${id} has been successfully deleted.`
+        });
+            
+
+            
             return res.status(409).json({ error: "Room cannot be deleted because it does not exist" });
         }
         res.status(200).json({ message: "Room deleted" });
